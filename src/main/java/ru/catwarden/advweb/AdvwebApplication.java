5a4ba6ecfb664856de2c124c6db2d58820660c72
CatWarden09 <CatWarden09@proton.me>
2026-02-11 01:56:00 +0400

    Initial commit

---
+++ src/main/java/ru/catwarden/advweb/AdvwebApplication.java
@@ -0,0 +1,13 @@
+package ru.catwarden.advweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/catwarden/advweb/AdvwebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.catwarden.advweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
