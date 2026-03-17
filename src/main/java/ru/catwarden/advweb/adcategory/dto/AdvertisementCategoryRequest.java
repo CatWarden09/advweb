a7e03a5761937edfb31b1331975a40963b5d5d8c
CatWarden09 <CatWarden09@proton.me>
2026-03-18 01:33:11 +0400

    Updated AdCategory DTO
    Removed the unused parent id since it's provided through path variable or is set to null if the parent category is created.

--- src/main/java/ru/catwarden/advweb/adcategory/dto/AdvertisementCategoryRequest.java
+++ src/main/java/ru/catwarden/advweb/adcategory/dto/AdvertisementCategoryRequest.java
@@ -1,7 +1,6 @@
 package ru.catwarden.advweb.adcategory.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,8 +15,5 @@ public class AdvertisementCategoryRequest {
     @NotBlank
     @Size(min = 3, max = 100)
     private String name;
-
-    private Long parentId;
-
 }
 
