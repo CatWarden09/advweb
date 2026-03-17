a7f1c6f91dfce2be6b79ff3812b14659552e712b
CatWarden09 <CatWarden09@proton.me>
2026-03-17 14:18:03 +0400

    Add InvalidStateException
    Introduced new Exception class for handling business logic entity state exceptions

---
+++ src/main/java/ru/catwarden/advweb/exception/InvalidStateException.java
@@ -0,0 +1,7 @@
+package ru.catwarden.advweb.exception;
+
+public class InvalidStateException extends RuntimeException {
+    public InvalidStateException(String message) {
+        super(message);
+    }
+}
