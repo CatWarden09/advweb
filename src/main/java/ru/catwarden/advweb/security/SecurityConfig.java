3a50fae5ecef50f4e9e18122d7b9390ad519815f
CatWarden09 <CatWarden09@proton.me>
2026-03-17 22:02:19 +0400

    Add endpoint security permissions
    Updated the permissions sctructure in SecurityConfig

--- src/main/java/ru/catwarden/advweb/security/SecurityConfig.java
+++ src/main/java/ru/catwarden/advweb/security/SecurityConfig.java
@@ -23,12 +23,33 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/*").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/*/advertisements").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/*/reviews/received").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/*/reviews/authored/approved").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/advertisements/**").permitAll()
+
+                        .requestMatchers("/users/*/reviews/received").permitAll()
+                        .requestMatchers("/users/*/advertisements").permitAll()
+
+
                         .requestMatchers("/moderation/**").hasRole("ADMIN")
+
+                        .requestMatchers("/reviews/**").hasRole("USER")
+
+                        .requestMatchers("/images/**").hasRole("USER")
+
+                        .requestMatchers("/comments/**").hasRole("USER")
+
+                        .requestMatchers("/avatars/**").hasRole("USER")
+
+                        .requestMatchers(HttpMethod.POST, "/advertisements/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PATCH, "/advertisements/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/advertisements/**").hasRole("USER")
+
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        .requestMatchers("/users/**").hasRole("USER")
+
+
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
